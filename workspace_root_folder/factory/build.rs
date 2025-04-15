8f3f3de559173482ac3cc82fbb683cf55ed2a6ae
dj8yf0μl <noreply@nowhere.org>
2025-04-15 23:29:49 +0300

    test simple build script

--- workspace_root_folder/factory/build.rs
+++ workspace_root_folder/factory/build.rs
@@ -1,42 +1,54 @@
-use std::str::FromStr;
-
-use cargo_near_build::{bon, camino, extended};
-use cargo_near_build::BuildOpts;
+use sha2::{Digest, Sha256};
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let _e = env_logger::Builder::new().parse_default_env().try_init();
-
     // directory of target sub-contract's crate
     let workdir = "../product-donation";
     // unix path to target sub-contract's crate from root of the repo
     let nep330_contract_path = "workspace_root_folder/product-donation";
 
-    let manifest = camino::Utf8PathBuf::from_str(workdir)
-        .expect("pathbuf from str")
-        .join("Cargo.toml");
-    let build_opts = BuildOpts::builder()
-        .manifest_path(manifest)
-        .override_nep330_contract_path(nep330_contract_path)
-        // a distinct target is needed to avoid deadlock during build
-        .override_cargo_target_dir("../target/build-rs-product-donation")
-        .build();
+    let override_cargo_target_dir = {
+        let out_dir_env = std::env::var("OUT_DIR").expect("OUT_DIR is always set in build scripts");
+        let out_dir = Path::new(&out_dir_env);
 
+        out_dir.join(format!("target-{}-for-{}", "product", "factory"))
+    };
 
-    let build_script_opts = extended::BuildScriptOpts::builder()
-        .rerun_if_changed_list(bon::vec![workdir, "../Cargo.toml", "../Cargo.lock",])
-        .build_skipped_when_env_is(vec![
-            // shorter build for `cargo check`
-            ("PROFILE", "debug"),
-            (cargo_near_build::env_keys::BUILD_RS_ABI_STEP_HINT, "true"),
-        ])
-        .stub_path("../target/stub.bin")
-        .result_env_key("BUILD_RS_SUB_BUILD_ARTIFACT_1")
-        .build();
+    let mut cmd = std::process::Command::new("cargo");
 
-    let extended_opts = extended::BuildOptsExtended::builder()
-        .build_opts(build_opts)
-        .build_script_opts(build_script_opts)
-        .build();
+    cmd.args(["near", "build", "non-reproducible-wasm", "--locked"]);
 
-    cargo_near_build::extended::build(extended_opts).expect("sub-contract build error");
+    cmd.current_dir(workdir);
+    cmd.env("CARGO_TARGET_DIR", &override_cargo_target_dir);
+    cmd.env("NEP330_BUILD_INFO_CONTRACT_PATH", nep330_contract_path);
+
+    cmd.status().expect("no problems in sub-build");
+
+    let out_path =
+        override_cargo_target_dir.join("near/simple_factory_product/simple_factory_product.wasm");
+
+    for in_path in vec![workdir, "../Cargo.toml", "../Cargo.lock"] {
+        println!("cargo::rerun-if-changed={}", in_path);
+    }
+    println!(
+        "cargo::rustc-env={}={}",
+        "BUILD_RS_SUB_BUILD_ARTIFACT_1",
+        out_path.to_str().expect("valid utf8")
+    );
+    println!(
+        "cargo::warning={}",
+        format!("out path is {}", out_path.to_str().expect("valid utf8"))
+    );
+    println!(
+        "cargo::warning={}",
+        format!("sha256 is {}", compute_hash(&out_path))
+    );
+}
+
+pub fn compute_hash(path: &PathBuf) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(std::fs::read(&path).expect("path read"));
+    let hash = hasher.finalize();
+    let hash: &[u8] = hash.as_ref();
+    bs58::encode(&hash).into_string()
 }
