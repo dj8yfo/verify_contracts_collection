5ae7c04be7c5a6b356d22161305a167698267c07
dj8yf0μl <noreply@nowhere.org>
2025-04-17 15:06:11 +0300

    use parse output to get out path of cargo-near subprocess

--- workspace_root_folder/factory/build.rs
+++ workspace_root_folder/factory/build.rs
@@ -1,26 +1,40 @@
 use std::path::Path;
+pub fn is_abi_build_step_or_debug_profile() -> bool {
+    if let Ok(value) = std::env::var("CARGO_NEAR_ABI_GENERATION") {
+        if value == "true" {
+            return true;
+        }
+    }
+    if let Ok(value) = std::env::var("PROFILE") {
+        if value == "debug" {
+            return true;
+        }
+    }
+    false
+}
 
+fn override_cargo_target_dir() -> std::path::PathBuf {
+    let out_dir_env = std::env::var("OUT_DIR").expect("OUT_DIR is always set in build scripts");
+    let out_dir = Path::new(&out_dir_env);
+
+    let dir = out_dir.join(format!("target-{}-for-{}", "product", "factory"));
+
+    std::fs::create_dir_all(&dir).expect("create dir");
+    dir
+}
 fn main() {
     // directory of target sub-contract's crate
     let workdir = "../product-donation";
-    // unix path to target sub-contract's crate from root of the repo
+    // unix path to `workdir` from root of the repo
     let nep330_contract_path = "workspace_root_folder/product-donation";
 
     // this is wasm result path of `cargo near build non-reproducible-wasm`
     // for target sub-contract, where repo's root is replaced with `/home/near/code`
     let nep330_output_wasm_path: &str = "/home/near/code/workspace_root_folder/target/near/simple_factory_product/simple_factory_product.wasm";
 
-    let override_cargo_target_dir = {
-        let out_dir_env = std::env::var("OUT_DIR").expect("OUT_DIR is always set in build scripts");
-        let out_dir = Path::new(&out_dir_env);
+    let override_cargo_target_dir = override_cargo_target_dir();
 
-        let dir = out_dir.join(format!("target-{}-for-{}", "product", "factory"));
-
-        std::fs::create_dir_all(&dir).expect("create dir");
-        dir
-    };
-
-    let mut command = {
+    let command = {
         let mut cmd = std::process::Command::new("cargo");
         cmd.args(["near", "build", "non-reproducible-wasm", "--locked"]);
 
@@ -31,24 +45,15 @@ fn main() {
             "NEP330_BUILD_INFO_OUTPUT_WASM_PATH",
             nep330_output_wasm_path,
         );
+        cmd.env("NO_COLOR", "true");
+        cmd.stdout(std::process::Stdio::piped());
+        cmd.stderr(std::process::Stdio::piped());
         cmd
     };
 
     let is_abi_or_cargo_check = is_abi_build_step_or_debug_profile();
 
-    let out_path = if is_abi_or_cargo_check {
-        override_cargo_target_dir.join("empty_subcontract_stub.wasm")
-    } else {
-        // this is wasm result path of `cargo near build non-reproducible-wasm`,
-        // for target sub-contract, subpath after `target`
-        override_cargo_target_dir.join("near/simple_factory_product/simple_factory_product.wasm")
-    };
-
-    if is_abi_or_cargo_check {
-        std::fs::write(&out_path, b"").expect("success write");
-    } else {
-        command.status().expect("no problems in sub-build");
-    }
+    let out_path = run_build::step(command, is_abi_or_cargo_check, &override_cargo_target_dir);
 
     post_build::step(
         is_abi_or_cargo_check,
@@ -56,6 +61,46 @@ fn main() {
         out_path,
         "BUILD_RS_SUB_BUILD_ARTIFACT_1",
     );
+}
+
+mod run_build {
+
+    use std::path::PathBuf;
+    const RESULT_PREFIX: &str = "     -                Binary: ";
+
+    pub fn step(
+        mut command: std::process::Command,
+        is_abi_or_cargo_check: bool,
+        override_cargo_target_dir: &PathBuf,
+    ) -> PathBuf {
+        if is_abi_or_cargo_check {
+            let out_path = override_cargo_target_dir.join("empty_subcontract_stub.wasm");
+            std::fs::write(&out_path, b"").expect("success write");
+            out_path
+        } else {
+            let process = command.spawn().expect("could not spawn cargo-near");
+            let output = process
+                .wait_with_output()
+                .expect("waiting for cargo-near to finish");
+            if !output.status.success() {
+                panic!(
+                    "build with cargo-near failure: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
+
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            let result_line = stderr
+                .lines()
+                .filter(|x| x.starts_with(RESULT_PREFIX))
+                .last();
+
+            let out_path = result_line
+                .expect("no output line find in cargo-near output")
+                .strip_prefix(RESULT_PREFIX);
+            PathBuf::from(out_path.expect("starts with expected prefix").to_string())
+        }
+    }
 }
 
 mod post_build {
@@ -80,7 +125,7 @@ mod post_build {
             println!(
                 "cargo::warning={}",
                 format!(
-                    "subcontract empty stub is {}",
+                    "subcontract empty stub is `{}`",
                     out_path.to_str().expect("valid utf8")
                 )
             );
@@ -88,7 +133,7 @@ mod post_build {
             println!(
                 "cargo::warning={}",
                 format!(
-                    "subcontract out path is {}",
+                    "subcontract out path is `{}`",
                     out_path.to_str().expect("valid utf8")
                 )
             );
@@ -109,18 +154,4 @@ mod post_build {
         let hash: &[u8] = hash.as_ref();
         bs58::encode(&hash).into_string()
     }
-}
-
-pub fn is_abi_build_step_or_debug_profile() -> bool {
-    if let Ok(value) = std::env::var("CARGO_NEAR_ABI_GENERATION") {
-        if value == "true" {
-            return true;
-        }
-    }
-    if let Ok(value) = std::env::var("PROFILE") {
-        if value == "debug" {
-            return true;
-        }
-    }
-    false
 }
